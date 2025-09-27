a249ba544b5eac1e28ab32663f8b0999d332094b
Shivesh Tiwari <shiveshtiwari0@gmail.com>
2025-09-27 16:33:34 +0530

    fixed build issue in docs

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -122,10 +122,10 @@ const config: Config = {
         {
           title: 'More',
           items: [
-            {
-              label: 'Blog',
-              to: '/blog',
-            },
+            // {
+            //   label: 'Blog',
+            //   to: '/blog',
+            // },
             {
               label: 'GitHub',
               href: 'https://github.com/TheShiveshNetwork/create-express-app',
